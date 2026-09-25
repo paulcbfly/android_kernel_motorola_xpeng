751b68d7edba5e8218d9841437185e8cf3acebc7
paulcbfly <paulcbfly@users.noreply.github.com>
2026-09-26 03:29:13 +0800

    fs: replace SUSFS core files with AstideLabs v2.3
    Replace fs/susfs.c, include/linux/susfs.h, include/linux/susfs_def.h with versions from AstideLabs/android_kernel_xiaomi_sm8250 commit 0b8a115ddd41 (SuSFS v2.3.0 for 4.19.y).
    
    The v2.3 core switches to AS_FLAGS on inode->i_state and adds new sus_kstat helpers.

--- include/linux/susfs.h
+++ include/linux/susfs.h
@@ -9,7 +9,7 @@
 #include <linux/susfs_def.h>
 #include <linux/statfs.h>
 
-#define SUSFS_VERSION "v2.2.0"
+#define SUSFS_VERSION "v2.3.0"
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5,0,0)
 #define SUSFS_VARIANT "NON-GKI"
 #else
@@ -94,11 +94,13 @@ struct st_susfs_sus_kstat {
 };
 
 struct st_susfs_sus_kstat_hlist {
+	struct hlist_node                       node;
 	unsigned long                           target_ino;
 	unsigned long                           target_dev;
+	struct kstatfs                          spoofed_kstatfs;
+	int                                     spoofed_mnt_id;
 	bool                                    is_fuse;
 	struct st_susfs_sus_kstat               info;
-	struct hlist_node                       node;
 };
 #endif
 
@@ -137,6 +139,7 @@ struct st_susfs_open_redirect {
 };
 
 struct st_susfs_open_redirect_hlist {
+	struct hlist_node                       node;
 	unsigned long                           target_ino;
 	unsigned long                           target_dev;
 	unsigned long                           redirected_ino;
@@ -145,7 +148,6 @@ struct st_susfs_open_redirect_hlist {
 	struct kstatfs                          spoofed_kstatfs;
 	struct st_susfs_open_redirect           info;
 	bool                                    reversed_lookup_only;
-	struct hlist_node                       node;
 };
 #endif
 
